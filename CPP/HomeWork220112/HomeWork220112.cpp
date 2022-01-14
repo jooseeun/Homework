e40594f76034db8847221a72a07dfd41164619df
jooseeun <joo5484@gmail.com>
2022-01-14 13:12:19 +0900

    주세은 : Homework220112,220113

---
+++ CPP/HomeWork220112/HomeWork220112.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main()
+{
+    { //짝수로 채우기
+        int Arr[10] = { 0, };
+        int Index = 0;
+        while (Index != 10) {
+            //2 4 6 8 10 12 14 16 18 20
+            Arr[Index] = 2 * (Index + 1);
+            Index += 1;
+        }
+        int a = 0;
+    }
+    { //홀수로 채우기
+        int Arr[10] = { 0, };
+        int Index = 0;
+        while (Index != 10) {
+            //1 3 5 7 9 11 13 15 17 19
+            Arr[Index] = Index * 2 + 1;
+            Index += 1;
+        }
+        int a = 0;
+    }
+}
---
+++ CPP/HomeWork220113/HomeWork220113.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+void StringTrimRemove(const char* _Left, char* _Right) {
+    int countL = 0;
+    int countR = 0;
+    while (0 != _Left[countL]) {
+        if (_Left[countL] != ' ') {
+            _Right[countR] = _Left[countL];
+            countR += 1;
+        }
+        countL += 1;
+    }
+    _Right[countR] = _Left[countL];
+}
+
+int main()
+{
+    char Text[1024] = { 0 };
+    StringTrimRemove("a a a              b b b c c c d d d", Text);
+    //"aaabbbcccddd"
+    printf_s(Text);
+
+}
