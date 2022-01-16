5d0dbe3a285b332bbf873c19a901642e7fcadf25
jooseeun <joo5484@gmail.com>
2022-01-16 19:19:34 +0900

    주세은 : HomeWork220114

---
+++ CPP/HomeWork220114CharChange/HomeWork220114CharChange.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+int StringLen(const char* _Ptr)
+{
+	int Count = 0;
+
+	while (0 != _Ptr[Count])
+	{
+		Count += 1;
+	}
+
+	return Count;
+}
+
+void CharChange(char* _Text, char _Prev, char _Next) {
+	int Len = StringLen(_Text);
+	for (int i = 0; i < Len; i += 1) {
+		if (_Text[i] == _Prev) {
+			_Text[i] = _Next;
+		}
+	}
+}
+
+
+int main()
+{char Text[100] = "aa, bb, cc, dd";
+
+		CharChange(Text, ',', '|');
+		// aa| bb| cc| dd
+		printf_s(Text);
+}
