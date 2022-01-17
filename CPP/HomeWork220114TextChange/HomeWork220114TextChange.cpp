d86f72a683f32085544ca7bd8b37f0434d65f13a
jooseeun <joo5484@gmail.com>
2022-01-17 23:19:29 +0900

    주세은 : HomeWork220117

---
+++ CPP/HomeWork220114TextChange/HomeWork220114TextChange.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+int StringLen(const char* _Ptr)
+{
+	int Count = 0;
+
+	while (0 != _Ptr[Count])
+	{
+		Count += 1;
+	}
+
+	return Count;
+}
+void TextChange(char* _Text, const char* _Prev, const char* _Next)
+{
+	int Len = StringLen(_Text);
+	
+	for (int i = 0; i < Len; i++)
+	{
+		if (_Text[i] == _Prev[0] && _Text[i + 1] == _Prev[1]) {
+			Len += 1;
+			_Text[Len] = 'k';
+			for (int j = Len-1; j ==i; j-=1)
+			{
+				_Text[j] = _Text[j-1];
+			}
+			_Text[i] = _Next[0];
+			_Text[i + 1] = _Next[1];
+			_Text[i + 2] = _Next[2];
+		}
+
+	}
+}
+int main()
+{
+	char Text[100] = "aa, bb, aa, bb aa aa aa aaaaaaa ccdffeds";
+
+	TextChange(Text, "aa", "fff");
+	// "fff, bb, fff, bb fff fff fff fffffffffa ccdffeds";
+	// aa| bb| cc| dd
+	printf_s(Text);
+}
---
+++ CPP/HomeWork220117/HomeWork220117.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+
+int StringLen(const char* _Ptr)
+{
+    int Count = 0;
+
+    while (0 != _Ptr[Count])
+    {
+        Count += 1;
+    }
+
+    return Count;
+}
+int CountFirst(const char* _Text, int _Start, const char* _FindStr)
+{
+    int Len = StringLen(_Text);
+    for (int i = 0; i < Len; i++)
+    {
+        if (_Text[i]==_FindStr[0]&& _Text[i+1] == _FindStr[1]&& _Text[i+2] == _FindStr[2])
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int CountLast(const char* _Text, int _End, const char* _FindStr)
+{
+    int count = 0;
+    int Len = StringLen(_Text);
+    for (int i = Len-3; i >1; i--)
+    {
+        if (_Text[i] == _FindStr[0] && _Text[i + 1] == _FindStr[1] && _Text[i + 2] == _FindStr[2])
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+
+int main()
+{
+
+    {
+        int Count = CountFirst("aaa eee ttt asdfasd eee", 15, "eee");
+        int a = 0;
+    }
+    {
+        int Count = CountLast("aaa eee asdfasd eee", 15, "eee");
+        int a = 0;
+    }
+
+
+
+}
