efcd8405ac7f10a750997735ac5041cb0e368fd1
jooseeun <joo5484@gmail.com>
2022-02-14 11:44:24 +0900

    주세은 : Homework 220211

--- CPP/HomeWork220211/소스.cpp
+++ CPP/HomeWork220211/소스.cpp
@@ -2,7 +2,7 @@
 
 class mt1993 {
 public:
-	
+
 	mt1993(ran_dev _Test2) {
 
 	}
@@ -12,14 +12,14 @@ public:
 class ran_dev {
 public:
 	ran_dev& operator()() {
-		
+
 		return *this;
 
 	}
 };
 
 int main() {
-	
+
 	ran_dev _Test1;
 	mt1993 _Test2(_Test1());
 
