d5130baf6da4113df2cbffca4d9f17bf735de5dd
jooseeun <joo5484@gmail.com>
2022-02-14 11:40:58 +0900

    주세은 : Homework 220211

---
+++ CPP/Homework220214/homework.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+class mt1993 {
+public:
+	
+	mt1993(ran_dev _Test2) {
+
+	}
+
+};
+
+class ran_dev {
+public:
+	ran_dev& operator()() {
+		
+		return *this;
+
+	}
+};
+
+int main() {
+	
+	ran_dev _Test1;
+	mt1993 _Test2(_Test1());
+
+}
