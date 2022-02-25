8676a9c048ba060a759a2d5953489eb385061c77
jooseeun <joo5484@gmail.com>
2022-02-25 12:15:57 +0900

    주세은 : Homework 220224

--- CPP/HomeWork220224/GameMap.cpp
+++ CPP/HomeWork220224/GameMap.cpp
@@ -83,11 +83,11 @@ private:
         {
             if (nullptr != LeftChild_)
             {
-                LeftChild_->MidOrder();
+                LeftChild_->LastOrder();
             }
             if (nullptr != RightChild_)
             {
-                RightChild_->MidOrder();
+                RightChild_->LastOrder();
             }
             // 내가 해야할일
             std::cout << Pair_.first << std::endl;
